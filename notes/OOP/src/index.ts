2ac739c0290fdf55ec368399f64658b87081c3fb
feliph <feliphrenaud@gmail.com>
2024-07-05 15:28:49 -0300

    commit de atualização dos estudos

--- notes/OOP/src/index.ts
+++ notes/OOP/src/index.ts
@@ -3,23 +3,21 @@
 //criando o esqueleto do app
 
 class Users {
-  name: string
-  private balance: number = 0
+  name: string;
+  private balance: number = 0;
 
   constructor(n: string, b: number) {
-    this.name = n
-    this.balance = b
+    this.name = n;
+    this.balance = b;
   }
 
   addMoney(amount: number) {
-    this.balance += amount
+    this.balance += amount;
   }
-
 }
 
-const user1 = new Users(`Feliph`, 10)
-user1.addMoney(100)
-const user2 = new Users(`marcos`, 20)
+const user1 = new Users(`Feliph`, 10);
+user1.addMoney(100);
+const user2 = new Users(`marcos`, 20);
 console.log(user1);
 console.log(user2);
-
