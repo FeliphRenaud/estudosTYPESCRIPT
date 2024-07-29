a29e9aa468c6947926dbed11197189306f35a135
feliph <feliphrenaud@gmail.com>
2024-07-29 14:15:18 -0300

    commit de atualização do conteudo de Functions dentro da pasta sections

---
+++ sections/functions/src/index.ts
@@ -0,0 +1,137 @@
+// 1 Void ( retorno para funções que nao retornam nada )
+
+function withoutReturn(): void {
+  console.log(`Esta Função não tem retorno `);
+}
+
+withoutReturn();
+
+// callback como argumento
+
+function greeting(name: string): string {
+  return `Olá ${name}`;
+}
+
+function preGreeting(f: (name: string) => string, userName: string) {
+  console.log(`Preparando a função `);
+
+  const greet = f(userName);
+
+  console.log(greet);
+}
+
+preGreeting(greeting, `Feliph`);
+
+// 3 generic function
+
+function firstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+console.log(firstElement([1, 2, 3, 4]));
+console.log(firstElement([`a`, `b`, `c`, `d`]));
+
+function mergeObjects<U, T>(obj1: U, obj2: T) {
+  return {
+    ...obj1,
+    ...obj2,
+  };
+}
+
+const newObject = mergeObjects(
+  { name: `Feliph` },
+  { Age: 30, job: `Programmer` }
+);
+console.log(newObject);
+
+// 4 constraints
+//MAIS INDICADO PARA UTILIZAR QUANDO ESTAMOS CRIANDO ALGO
+
+function biggestNumber<T extends number | string>(a: T, b: T): T {
+  let biggest: T;
+  if (+a > +b) {
+    biggest = a;
+  } else {
+    biggest = b;
+  }
+
+  return biggest;
+}
+
+console.log(biggestNumber(5, 3));
+console.log(biggestNumber(`12`, `5`));
+
+// especificação de argumentos
+// MIAS INDICADO PARA UTILIZAR QUANDO ESTAMOS LIDADANDO COM ALGO PRONTO DE TERCEIROS
+
+function mergeArrays<T>(arr1: T[], arr2: T[]) {
+  return arr1.concat(arr2);
+}
+
+console.log(mergeArrays<number | string>([1, 2, 3], [`teste`, `testando`]));
+
+//6 parametros opcionais
+
+function modernGreeting(name: string, greet?: string) {
+  if (greet) {
+    return `Ola ${greet} ${name}, tudo bem?`;
+  }
+  return `Ola ${name}, tudo bem ? `;
+}
+
+console.log(modernGreeting(`Feliph`));
+console.log(modernGreeting(`Feliph`, `Mst`));
+
+// 7 PARAMETROS OPCIONAIS
+// 7.1 DEFAULT
+
+function somaDefault(n: number, m = 10) {
+  return n + m;
+}
+console.log(somaDefault(10));
+console.log(somaDefault(15, 12));
+
+// 7.2 UNKNOWN
+
+function doSomething(x: unknown) {
+  if (Array.isArray(x)) {
+    console.log(x[0]);
+  } else if (typeof x === `number`) {
+    console.log(`X é um numero = ${x}`);
+  }
+}
+
+doSomething(1);
+
+doSomething([1, 2, 3]);
+
+// 7.3 NEVER
+// semelhante ao void ele nao retorna nada
+
+function showErrorMessage(msg: string): never {
+  throw new Error(msg);
+}
+//showErrorMessage(`Algum erro`);
+
+//7.4 REST OPERATOR
+
+function sumAll(...n: number[]) {
+  return n.reduce((number, sum) => sum + number);
+}
+console.log(sumAll(1, 2, 3, 4, 5));
+console.log(sumAll(5, 348, 2348));
+//console.log(sumAll(`teste`));
+
+//8 Destructuring como parametro
+
+function showProductDetails({
+  name,
+  price,
+}: {
+  name: string;
+  price: number;
+}): string {
+  return `O nome do produto é ${name} e ele custa R$ ${price}`;
+}
+const shirt = { name: `Camisa`, price: 49.99 };
+console.log(showProductDetails(shirt));
